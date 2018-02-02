3cdc5ea9e508dfe231e0637cc2e7581c72938a51
T6o <t6o.mrc@gmail.com>
2018-02-02 17:20:48 +0100

    Update production.js

--- backend/config/env/production.js
+++ backend/config/env/production.js
@@ -8,7 +8,7 @@ module.exports = {
 	db : 'mongodb://localhost/Mern-production',
 	sessionSecret: 'productionSessionSecret',
 	expiresIn:'15m',
-	sendgrid:'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',
+	sendgrid:'',
 
 
 };
